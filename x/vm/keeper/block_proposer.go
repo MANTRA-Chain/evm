8e12403546251892a97d299a36357a7097c185d4
mmsqe <tqd0800210105@gmail.com>
2026-01-06 12:18:14 +0800

    Revert "fix coinbase"
    This reverts commit 63b71d2505f24d7e867c69099c393ef78ccad390.

--- x/vm/keeper/block_proposer.go
+++ x/vm/keeper/block_proposer.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	errorsmod "cosmossdk.io/errors"
 	"github.com/ethereum/go-ethereum/common"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -15,15 +14,8 @@ func (k Keeper) GetCoinbaseAddress(ctx sdk.Context, proposerAddress sdk.ConsAddr
 		return common.Address{}, nil
 	}
 
-	bz, err := sdk.ValAddressFromBech32(validator.GetOperator())
-	if err != nil {
-		return common.Address{}, errorsmod.Wrapf(
-			err,
-			"failed to convert validator operator address %s to bytes",
-			validator.GetOperator(),
-		)
-	}
-	return common.BytesToAddress(bz), nil
+	coinbase := common.BytesToAddress([]byte(validator.GetOperator()))
+	return coinbase, nil
 }
 
 // GetProposerAddress returns current block proposer's address when provided proposer address is empty.
