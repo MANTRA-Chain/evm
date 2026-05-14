2ae96163c6d085b245718fe7126814b49c1363de
mmsqe <tqd0800210105@gmail.com>
2026-05-14 12:55:17 +0800

    fix test

--- x/vm/keeper/grpc_query_test.go
+++ x/vm/keeper/grpc_query_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	storetypes "github.com/cosmos/cosmos-sdk/store/v2/types"
+	storetypes "cosmossdk.io/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
