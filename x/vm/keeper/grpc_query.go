e6d17887db0a7a93be74d73012ec7c4653db96bc
mmsqe <tqd0800210105@gmail.com>
2026-05-15 11:57:22 +0800

    preserve parent ctx kv gas config

--- x/vm/keeper/grpc_query.go
+++ x/vm/keeper/grpc_query.go
@@ -542,8 +542,7 @@ func (k Keeper) TraceTx(c context.Context, req *types.QueryTraceTxRequest) (*typ
 	// need to reset gas meter per transaction to be consistent with tx execution
 	// and avoid stacking the gas used of every predecessor in the same gas meter
 
-	ctx = evmante.BuildEvmExecutionCtx(ctx).
-		WithGasMeter(storetypes.NewGasMeter(maxPredecessorGas))
+	ctx = ctx.WithGasMeter(storetypes.NewGasMeter(maxPredecessorGas))
 	for i, tx := range req.Predecessors {
 		ethTx := tx.AsTransaction()
 		msg, err := core.TransactionToMessage(ethTx, signer, cfg.BaseFee)
